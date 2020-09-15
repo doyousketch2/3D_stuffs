f20330ed3eecc421a0675c04b6a96f458be1f0ad
doyousketch2 <doyousketch2@yahoo.com>
2020-09-14 21:45:30 -0400

    Update topnav.js

--- scripts/topnav.js
+++ scripts/topnav.js
@@ -15,17 +15,17 @@ $(  function() {
 
         var target = $( event.target );
 
-        if      ( target .is( "button#Head" ) )  {  $( "nav#sliders" ) .html( "head.html" );  }
-        else if ( target .is( "button#Torso" ) )  {  $( "nav#sliders" ) .html( "torso.html" );  }
+        if      ( target .is( "button#Head" ) )  {  $( "nav#sliders" ) .html( "head.html" ) .load( "scripts/head.html" );  }
+        else if ( target .is( "button#Torso" ) )  {  $( "nav#sliders" ) .html( "torso.html" ) .load( "scripts/torso.html" );  }
 
-        else if ( target .is( "button#Arms" ) )  {  $( "nav#sliders" ) .load( "/scripts/arms.html" );  }
-        else if ( target .is( "button#Hands" ) )  {  $( "nav#sliders" ) .html( "hands.html" );  }
+        else if ( target .is( "button#Arms" ) )  {  $( "nav#sliders" ) .html( "arms.html" ) .load( "scripts/arms.html" );  }
+        else if ( target .is( "button#Hands" ) )  {  $( "nav#sliders" ) .html( "hands.html" ) .load( "scripts/hands.html" );  }
 
-        else if ( target .is( "button#Legs" ) )  {  $( "nav#sliders" ) .html( "legs.html" );  }
-        else if ( target .is( "button#Feet" ) )  {  $( "nav#sliders" ) .html( "feet.html" );  }
+        else if ( target .is( "button#Legs" ) )  {  $( "nav#sliders" ) .html( "legs.html" ) .load( "scripts/legs.html" );  }
+        else if ( target .is( "button#Feet" ) )  {  $( "nav#sliders" ) .html( "feet.html" ) .load( "scripts/feet.html" );  }
 
-        else if ( target .is( "button#Camera" ) )  {  $( "nav#sliders" ) .html( "camera.html" );  }
-        else if ( target .is( "button#Accessories" ) )  {  $( "nav#sliders" ) .html( "accessories.html" );  };
+        else if ( target .is( "button#Camera" ) )  {  $( "nav#sliders" ) .html( "camera.html" ) .load( "scripts/camera.html" );  }
+        else if ( target .is( "button#Accessories" ) )  {  $( "nav#sliders" ) .html( "accessories.html" ) .load( "scripts/accessories.html" );  };
     }  );
 
 //================================================================================================//
