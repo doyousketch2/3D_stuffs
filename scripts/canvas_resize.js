bc2c585ec2c7234e3021513d3e5f10ec19685c1b
doyousketch2 <doyousketch2@yahoo.com>
2020-09-14 22:43:17 -0400

    Update canvas_resize.js

--- scripts/canvas_resize.js
+++ scripts/canvas_resize.js
@@ -1,6 +1,4 @@
-$(  function() {
-
-//================================================================================================//
+function canvas_size() {
 
     var horiz_padding  = 35;
     var vert_padding  = 25;
@@ -12,13 +10,18 @@ $(  function() {
     var available_height  = $( document ) .height() -used_vert_space;
 
     var available  = available_width;
-    if ( available_width > available_height ) {  var available  = available_height;  }
+    if ( available_width > available_height ) {  available  = available_height;  }
 
     $( "#cloth" ) .width( available ) .height( available );
 
     $( "#Info1" ) .html( "used_vertical_space: " +used_vert_space +"  used_horizontal_space: " +used_horiz_space );
     $( "#Info2" ) .html( "available_width: " +available_width +"  available_height: " +available_height +"   available: " +available );
 
-//  eof  =========================================================================================//
+}
 
-}  );
+//================================================================================================//
+
+$( canvas_size );  //  onload
+$( window ) .resize( canvas_size );
+
+//  eof  =========================================================================================//
