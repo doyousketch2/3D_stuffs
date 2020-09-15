ff9bb777587330ad6c95d002e4561f9d2f7e5ac8
doyousketch2 <doyousketch2@yahoo.com>
2020-09-14 23:13:28 -0400

    Update topnav.js

--- scripts/topnav.js
+++ scripts/topnav.js
@@ -1,6 +1,4 @@
-$(  function() {
-
-//================================================================================================//
+$(  function() {  //  onload
 
     $( "#topnav button" ) .hover(  function() {
             $( this ) .css( "border", "3px solid #789" ) .css( "padding", "6px" );
@@ -8,6 +6,7 @@ $(  function() {
             $( this ) .css( "border", "none" ) .css( "padding", "9px" );
     }  );
 
+    //============================================================================================//
 
     $( "#topnav button" ) .click(  function( event ) {
         $( "#topnav button" ) .css( "background-color", "#123" ) .css( "color", "#DEF" );
@@ -26,12 +25,41 @@ $(  function() {
 
         else if ( target .is( "button#Camera" ) )  {  $( "nav#sliders" ) .html( "camera.html" ) .load( "scripts/camera.html" );  }
         else if ( target .is( "button#Accessories" ) )  {  $( "nav#sliders" ) .html( "accessories.html" ) .load( "scripts/accessories.html" );  };
+
+        var minimum  = 0,
+            maximum  = 100,
+            half = ( maximum -minimum ) /2;
+
+        //========================================================================================//
+
+        $( ".slider" ) .each(  function( index ) {
+            $( this ) .slider(  {
+                range: "max",
+		            min: minimum,
+              	max: maximum,
+             	  value: half,
+                slide: function( event, ui ) {
+                    $( this ) .prev( "input.info" ) .val( ui.value );  }
+            }  );
+
+            $( this ) .prev( "input.info" ) .val(
+                $( this ) .slider( "value" )  );
+        }  );
+
+        //========================================================================================//
+
+        $( ".info" ) .keyup( function () {
+            if ( this .value < minimum ) { this .value = minimum }
+            else if ( this .value > maximum ) { this .value = maximum };
+
+            $( this ) .next( "div" ) .slider( "value",  this .value );
+        } );
     }  );
 
-//================================================================================================//
+    //============================================================================================//
 
     $( "#footer" ) .width(  $( "#topnav" ) .width()  );
 
-//  eof  =========================================================================================//
-
 }  );
+
+//  eof  =========================================================================================//
